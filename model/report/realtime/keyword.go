0fa8aec4a56d9db23a6e563c908780f41b69c383
Syd Xu <prof.syd.xu@gmail.com>
2024-02-28 18:47:51 +0800

    fix(report/realtime): AdvertiserID in realtime report request from string to uint64

--- model/report/realtime/keyword.go
+++ model/report/realtime/keyword.go
@@ -10,7 +10,7 @@ import (
 // KeywordRequest 关键词层级实时数据 API Request
 type KeywordRequest struct {
 	// AdvertiserID 广告主ID
-	AdvertiserID string `json:"advertiser_id"`
+	AdvertiserID uint64 `json:"advertiser_id"`
 	// StartDate 开始时间，格式 yyyy-MM-dd
 	StartDate string `json:"start_date,omitempty"`
 	// EndDate 结束时间，格式 yyyy-MM-dd
